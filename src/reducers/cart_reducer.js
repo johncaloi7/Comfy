1ac0151c41be3e1cbc625823ae7c02973a04b9ac
unknown <pinlac.johncarlo@gmail.com>
2022-04-19 20:48:26 +0800

    cart context & reducers complete

--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -4,11 +4,89 @@ import {
   COUNT_CART_TOTALS,
   REMOVE_CART_ITEM,
   TOGGLE_CART_ITEM_AMOUNT,
-} from '../actions'
+} from "../actions";
 
 const cart_reducer = (state, action) => {
-  return state
-  throw new Error(`No Matching "${action.type}" - action type`)
-}
+  switch (action.type) {
+    case ADD_TO_CART:
+      const { id, amount, color, product } = action.payload;
+      const tempItem = state.cart.find((i) => i.id === id + color);
 
-export default cart_reducer
+      if (tempItem) {
+        const tempCart = state.cart.map((cartItem) => {
+          if (cartItem.id === id + color) {
+            let newAmount = cartItem.amount;
+            if (newAmount > cartItem.max) {
+              newAmount = cartItem.max;
+            }
+            return { ...cartItem, amount: newAmount };
+          } else {
+            return cartItem;
+          }
+        });
+        return { ...state, cart: tempCart };
+      } else {
+        const newItem = {
+          id: id + color,
+          name: product.name,
+          color,
+          amount,
+          image: product.images[0].url,
+          price: product.price,
+          max: product.stock,
+        };
+        return { ...state, cart: [...state.cart, newItem] };
+      }
+
+    case REMOVE_CART_ITEM:
+      const tempCart = state.cart.filter((item) => {
+        return item.id !== action.payload;
+      });
+
+      return { ...state, cart: tempCart };
+
+    case CLEAR_CART:
+      return { ...state, cart: [] };
+
+    case TOGGLE_CART_ITEM_AMOUNT:
+      const { id: itemID, value } = action.payload;
+      const toggledCart = state.cart.map((item) => {
+        if (item.id === itemID) {
+          if (value === "inc") {
+            let newAmount = item.amount + 1;
+            if (newAmount > item.max) {
+              newAmount = item.max;
+            }
+            return { ...item, amount: newAmount };
+          }
+          if (value === "dec") {
+            let newAmount = item.amount - 1;
+            if (newAmount < 1) {
+              newAmount = 1;
+            }
+            return { ...item, amount: newAmount };
+          }
+        }
+        return item;
+      });
+      return { ...state, cart: toggledCart };
+
+    case COUNT_CART_TOTALS:
+      const { total_items, total_amount } = state.cart.reduce(
+        (total, cartItem) => {
+          const { amount, price } = cartItem;
+          total.total_items += amount;
+          total.total_amount += price * amount;
+          return total;
+        },
+        { total_items: 0, total_amount: 0 }
+      );
+
+      return { ...state, total_amount, total_items };
+
+    default:
+      throw new Error(`No Matching "${action.type}" - action type`);
+  }
+};
+
+export default cart_reducer;
